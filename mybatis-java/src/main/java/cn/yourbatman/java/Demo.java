d67613f2aba5184a88ca5dea95788e7217ca72b0
YourBatman <shixiangfang@aliyun.com>
2020-10-20 06:36:00 +0800

    first commit

---
+++ mybatis-java/src/main/java/cn/yourbatman/java/Demo.java
@@ -0,0 +1,22 @@
+package cn.yourbatman.java;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        System.out.println(sqlSessionFactory);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        System.out.println(sqlSession);
+    }
+}
